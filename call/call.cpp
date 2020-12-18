4c9b4e145aa4522cfa324ea92144ecf4a179185f
Paul Raffer <paul.raffer@protonmail.com>
2020-12-18 21:44:20 +0100

    history is stored in repository 'raffer_cpplib-old'

---
+++ call/call.cpp
@@ -0,0 +1,83 @@
+
+// Copyright (c) 2020 Paul Raffer.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// https://www.boost.org/LICENSE_1_0.txt)
+
+#if __cplusplus >= 201703L
+
+#include "call.hpp"
+
+
+#include "raffer/system/system.hpp"
+
+
+namespace raffer
+{
+
+template <typename Char>
+auto call(
+	function_body<Char> const & this_,
+	ordered_functions<Char> const & functions,
+	basic_smatch<Char> const & arg)
+	-> std::basic_string<Char>
+{
+	if (std::holds_alternative<function_body_ptr<Char>>(this_))
+		return std::get<function_body_ptr<Char>>(this_)(arg);
+	else
+	{
+		auto ret = std::get<std::basic_string<Char>>(this_);
+	
+		for (auto fg = cbegin(functions); fg != cend(functions); ++fg)
+			for (auto func_found = true; is_up(raffer::key::esc) && func_found; )
+			{
+				auto func = function<Char>{};
+				auto next_match = basic_smatch<Char>{};
+	
+				func_found = false;
+				{
+				auto next_match_pos = std::numeric_limits<typename basic_smatch<Char>::difference_type>::max();
+				for (auto const & f : *fg)
+					if (regex_search(ret, next_match,
+						regex_namespace::basic_regex<Char>(f.first),
+						regex_namespace::regex_constants::format_first_only) &&
+						next_match.position() < next_match_pos)
+					{
+						func = f;
+						func_found = true;
+						if ((next_match_pos = next_match.position()) == 0)
+							break;
+					}
+				}
+	
+				if(func_found)
+				{
+					ret = regex_replace(ret,
+						regex_namespace::basic_regex<Char>(func.first),
+						call<Char>(func.second, functions, next_match),
+						regex_namespace::regex_constants::format_first_only);
+	
+					fg = begin(functions);
+				}
+			}
+	
+		return ret;
+	}
+}
+
+template auto call<char>(
+	function_body<char> const & this_,
+	ordered_functions<char> const & functions,
+	basic_smatch<char> const & arg)
+	-> std::basic_string<char>;
+
+template auto call<wchar_t>(
+	function_body<wchar_t> const & this_,
+	ordered_functions<wchar_t> const & functions,
+	basic_smatch<wchar_t> const & arg)
+	-> std::basic_string<wchar_t>;
+
+} // namespace raffer
+
+
+#endif // __cplusplus >= 201703L
---
+++ call/call.hpp
@@ -0,0 +1,82 @@
+
+// Copyright (c) 2020 Paul Raffer.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// https://www.boost.org/LICENSE_1_0.txt)
+
+#if __cplusplus >= 201703L
+
+#ifndef RAFFER_REGEX_CALL_CALL_HPP
+#define RAFFER_REGEX_CALL_CALL_HPP
+
+#define raffer_cpplib_regex_call__ 202006L
+
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <variant>
+#include <unordered_map>
+
+
+#include <regex>
+namespace regex_namespace = std;
+// for boost:
+//#include <boost/regex.hpp>
+//namespace regex_namespace = boost;
+
+
+namespace raffer // interface
+{
+
+template <typename Char>
+using basic_smatch =
+	regex_namespace::match_results<
+		typename std::basic_string<
+			Char>::const_iterator>;
+
+
+template <typename Char = char>
+using function_body_ptr =
+	std::function<
+		auto (
+			basic_smatch<Char> const &)
+			-> std::basic_string<Char>>;
+
+template <typename Char = char>
+using function_body =
+	std::variant<
+		std::basic_string<Char>,
+		function_body_ptr<Char>>;
+
+template <typename Char = char>
+using function =
+	std::pair<
+		std::basic_string<Char>,
+		function_body<Char>>;
+
+template <typename Char = char>
+using unordered_functions =
+	std::unordered_map<
+		std::basic_string<Char>,
+		function_body<Char>>;
+
+template <typename Char = char>
+using ordered_functions =
+	std::vector<
+		unordered_functions<Char>>;
+
+
+template <typename Char = char>
+auto call(
+	function_body<Char> const & this_,
+	ordered_functions<Char> const & functions,
+	basic_smatch<Char> const & arg)
+	-> std::basic_string<Char>;
+
+} // namespace raffer
+
+
+#endif // RAFFER_REGEX_CALL_CALL_HPP
+
+#endif // __cplusplus >= 201703L
